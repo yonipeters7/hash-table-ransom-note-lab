0dbf680ca0ad97fde67118b20878f0d991a845e8
Yonatan Peters <yonipeters7@gmail.com>
2026-01-02 11:44:34 -0500

    Implement can_construct function using hash table

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,21 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+    # Build a hash table (dictionary) to count character frequencies in magazine
+    char_count = {}
+    for char in magazine:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    # Check if each character in ransomNote is available in the hash table
+    for char in ransomNote:
+        # If character is missing or count is depleted, return False early
+        if char not in char_count or char_count[char] == 0:
+            return False
+        # Decrement the count as we "use" this character
+        char_count[char] -= 1
+
+    # All characters in ransomNote were found with sufficient frequency
+    return True
